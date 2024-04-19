33c89600745d5fa8d41883a2902594fe09fe5e4e
Shy11er <Ranil2086@bk.ru>
2024-04-20 01:53:52 +0500

    feat: add routes

---
+++ src/routes/event/get/index.ts
@@ -0,0 +1,13 @@
+import { FastifyInstance, FastifyRequest } from 'fastify';
+import schema from './schema';
+
+export default async (instance: FastifyInstance) => {
+  async function get(request: FastifyRequest<{ Params: { id: number } }>) {
+    const { eventService } = instance.dependencies.services;
+    const id = request.params.id;
+
+    return eventService.get(id);
+  }
+
+  instance.get('/', { schema }, get);
+};
