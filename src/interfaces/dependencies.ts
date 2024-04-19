5ec2228251fc173ae040cc81e5c23baab7fb6f7e
Shy11er <Ranil2086@bk.ru>
2024-04-20 01:52:34 +0500

    feat: add dependencies

--- src/interfaces/dependencies.ts
+++ src/interfaces/dependencies.ts
@@ -2,10 +2,12 @@ import { IConfig } from '@interfaces';
 import { PrismaClient } from '@prisma/client';
 import { TokenService, UserService } from '@services';
 import { Logger } from 'pino';
+import { EventService } from 'src/services/event.service';
 
 export interface IServices {
   userService: UserService;
   tokenService: TokenService;
+  eventService: EventService;
 }
 
 export interface IDependencies {
